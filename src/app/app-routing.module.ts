b936b9d5b506dd798565495c7a30038d2bb2651e
wendy1998-coder <wendyvandermeulen@gmail.com>
2020-09-22 12:40:24 +0200

    updated routing behaviour to also use url from browser

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,4 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {ProjectsComponent} from './components/projects/projects/projects.component';
@@ -7,6 +6,8 @@ import {HobbiesComponent} from './components/hobbies/hobbies.component';
 import {JobsComponent} from './components/jobs/jobs.component';
 import {SchoolingComponent} from './components/schooling/schooling.component';
 import {SkillsComponent} from './components/skills/skills/skills.component';
+import {LinkedinLoginResponseComponent} from "./components/skills/skill-support/login/linkedin-login-response/linkedin-login-response.component";
+import {LoginComponent} from "./components/skills/skill-support/login/login/login.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -15,13 +16,14 @@ const routes: Routes = [
   {path: 'hobbies', component: HobbiesComponent},
   {path: 'jobs', component: JobsComponent},
   {path: 'school', component: SchoolingComponent},
-  {path: 'skills', component: SkillsComponent}
+  {path: 'skills', component: SkillsComponent},
+  {path: 'linkedInLogin', component: LinkedinLoginResponseComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
     RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
 import { HeaderComponent } from './components/layout/header/header.component';
-import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { PhotoDisplayComponent } from './components/home/photo-display/photo-display.component';
 import { ProjectsComponent } from './components/projects/projects/projects.component';
@@ -15,6 +15,8 @@ import { JobsComponent } from './components/jobs/jobs.component';
 import { SchoolingComponent } from './components/schooling/schooling.component';
 import { SkillsComponent } from './components/skills/skills/skills.component';
 import { SkillsItemComponent } from './components/skills/skills-item/skills-item.component';
+import { LoginComponent } from './components/skills/skill-support/login/login/login.component';
+import { LinkedinLoginResponseComponent } from './components/skills/skill-support/login/linkedin-login-response/linkedin-login-response.component';
 
 @NgModule({
   declarations: [
@@ -29,12 +31,14 @@ import { SkillsItemComponent } from './components/skills/skills-item/skills-item
     JobsComponent,
     SchoolingComponent,
     SkillsComponent,
-    SkillsItemComponent
+    SkillsItemComponent,
+    LoginComponent,
+    LinkedinLoginResponseComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
